1f1fd365bfaacf196800a952086254e426eb5cac
zarboz <zarboz@gmail.com>
2012-08-07 10:18:56 -0700

    adding initial sweep2wake support

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Sun Aug 5 15:54:53 PDT 2012"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "EvilZ"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
